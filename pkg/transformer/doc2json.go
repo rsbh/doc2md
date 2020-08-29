9ea09437ce71e02f3b1df70400656464aaa495ad
Rishabh Mishra <me@rishabhmishra.dev>
2020-08-29 22:56:28 +0530

    Add function to return contents from doc reponse

--- pkg/transformer/doc2json.go
+++ pkg/transformer/doc2json.go
@@ -2,6 +2,7 @@ package transformer
 
 import (
 	"fmt"
+	"strings"
 
 	"google.golang.org/api/docs/v1"
 )
@@ -37,29 +38,62 @@ func getText(e *docs.ParagraphElement) string {
 	return text
 }
 
-func getParagraph(p *docs.Paragraph) {
-	t := p.ParagraphStyle.NamedStyleType
-	tag := tags[t]
+type M map[string]string
 
-	if tag != "" {
-		for _, e := range p.Elements {
-
-			tr := e.TextRun
-			if tr != nil {
-				headingID := p.ParagraphStyle.HeadingId
-				text := getText(e)
-				fmt.Println(headingID, text)
-			}
+func all(vs []M, f func(M) bool) bool {
+	for _, v := range vs {
+		if !f(v) {
+			return false
 		}
+	}
+	return true
+}
 
+func getTagContent(p *docs.Paragraph, tag string) []M {
+	var tagContent []M
+	for _, e := range p.Elements {
+
+		tr := e.TextRun
+		if tr != nil {
+			// headingID := p.ParagraphStyle.HeadingId
+			text := getText(e)
+			x := M{tag: text}
+			tagContent = append(tagContent, x)
+		}
+	}
+
+	if all(tagContent, func(m M) bool { return m[tag] != "" }) {
+		var a []string
+		for _, tc := range tagContent {
+			a = append(a, tc[tag])
+		}
+		s := strings.Join(a, " ")
+		s = strings.ReplaceAll(s, " .", ".")
+		s = strings.ReplaceAll(s, " ,", ",")
+		return []M{M{tag: s}}
+	} else {
+		return tagContent
 	}
 }
 
+func getParagraph(p *docs.Paragraph) []M {
+	var tc []M
+	t := p.ParagraphStyle.NamedStyleType
+	tag := tags[t]
+	if tag != "" {
+		tc = getTagContent(p, tag)
+	}
+	return tc
+}
+
 // DocToJSON Convert docs api response to json
-func DocToJSON(b *docs.Body) {
+func DocToJSON(b *docs.Body) []M {
+	var content []M
 	for _, s := range b.Content {
 		if s.Paragraph != nil {
-			getParagraph(s.Paragraph)
+			c := getParagraph(s.Paragraph)
+			content = append(content, c...)
 		}
 	}
+	return content
 }
